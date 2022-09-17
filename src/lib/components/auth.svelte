30c2c09245109f954ed7290defbfc2cf7e2ba1ec
idioticdev <hi@idiotic.dev>
2022-09-17 16:27:33 -0400

    add authentication interaction

---
+++ src/lib/components/auth.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import { supabase } from '$lib/services/supabase'
+    import { user } from '$lib/stores/user-store'
+
+    const on_sign_in = () => {
+        supabase.auth.signIn({
+            provider: 'github'
+        })
+    }
+
+    const on_sign_out = () => {
+        supabase.auth.signOut()
+    }
+</script>
+
+{#if $user}
+    <button on:click={on_sign_out}>Sign out</button>
+{:else}
+    <button on:click={on_sign_in}>Sign in</button>
+{/if}
