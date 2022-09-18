076c6c040298bdb3486fd52115aed90779035e5a
idioticdev <hi@idiotic.dev>
2022-09-17 21:20:29 -0400

    add components for grid layout

---
+++ src/lib/components/empty.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    export let title: string
+    export let description: string
+</script>
+
+<section class="empty-state">
+    {#if title}<h1 class="title">{title}</h1>{/if}
+    {#if description}<p>{description}</p>{/if}
+</section>
+
+<style>
+    .empty-state {
+        text-align: center;
+    }
+
+    .empty-state > .title {
+        font-size: 2rem;
+        font-weight: 600;
+        margin-bottom: 0.5rem;
+    }
+</style>
---
+++ src/lib/components/grid.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    export let tag: string = 'section'
+    export let place: 'left' | 'right' | 'center' = 'center'
+
+    $: classes = `grid-${tag} -${place}`
+</script>
+
+<svelte:element this={tag} class={classes}>
+    <slot />
+</svelte:element>
+
+<style>
+    :global([class^='grid-']) {
+        border-radius: var(--radius);
+        grid-row: 2;
+    }
+
+    :global(.grid-section.-center) {
+        place-self: center;
+        grid-column: 3;
+        width: 100%;
+        height: 100%;
+    }
+
+    :global(.grid-aside.-left) {
+        grid-column: 2;
+    }
+
+    :global(.grid-aside.-right) {
+        grid-column: 4;
+    }
+</style>
