ab792b7cdd78a97ce7ea84524d0e6f969daf03e1
idioticdev <hi@idiotic.dev>
2022-09-23 19:11:22 -0400

    add menu components

---
+++ src/lib/components/menu-item.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    export let heading = false
+</script>
+
+<li class="menu-item">
+    <svelte:element
+        this={heading ? 'h3' : 'a'}
+        class:heading
+        class:link={!heading}
+    >
+        <slot />
+    </svelte:element>
+</li>
+
+<style lang="scss">
+    .menu-item {
+        display: flex;
+        padding: 0;
+        margin: 0;
+        list-style: none;
+
+        > .link {
+            color: var(--color-foreground);
+            width: 100%;
+            display: flex;
+            padding: 0.6em 1.2em;
+            border: solid 2px transparent;
+            border-radius: var(--radius);
+            font-weight: 300;
+            cursor: pointer;
+            transition: all 300ms ease-in-out;
+
+            &:hover {
+                text-decoration: none;
+                box-shadow: inset 0 0 0 2px var(--color-border);
+            }
+        }
+    }
+</style>
